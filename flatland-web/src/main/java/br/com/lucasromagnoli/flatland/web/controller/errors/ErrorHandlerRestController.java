bc4805d44b460d55a7241475ff64bad67989f7de
Lucas Ramos Romagnoli <lucasr.romagnoli@gmail.com>
2020-01-26 22:09:29 -0200

    Incluindo o ExcpetionHandler para as exceções de validação.

--- flatland-web/src/main/java/br/com/lucasromagnoli/flatland/web/controller/errors/ErrorHandlerRestController.java
+++ flatland-web/src/main/java/br/com/lucasromagnoli/flatland/web/controller/errors/ErrorHandlerRestController.java
@@ -1,13 +1,24 @@
 package br.com.lucasromagnoli.flatland.web.controller.errors;
 
+import br.com.lucasromagnoli.javaee.underpinning.commons.exception.UnderpinningValidationException;
+import br.com.lucasromagnoli.javaee.underpinning.rest.model.MessageType;
+import br.com.lucasromagnoli.javaee.underpinning.rest.model.TemplateMessage;
+import br.com.lucasromagnoli.javaee.underpinning.rest.support.TemplateMessageSupport;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ErrorHandlerRestController {
 
-//    ** Exemplo **
-//    @ExceptionHandler(Exception.class)
-//    public ResponseEntity<String> handleExcpetion(Exception ex){
-//        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Tratando exceção");
-//    }
+    @ExceptionHandler(UnderpinningValidationException.class)
+    public ResponseEntity<TemplateMessage> handleExcpetion(UnderpinningValidationException ex){
+        return TemplateMessageSupport.begin()
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .messageType(MessageType.WARNING)
+                .payload(ex.getValidation())
+                .build()
+                .toResponseEntity();
+    }
 }
