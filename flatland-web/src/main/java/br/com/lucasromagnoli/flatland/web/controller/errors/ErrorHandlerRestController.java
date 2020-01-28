ea58d1ed7c953a090d83d8bfc438737c32713f30
Lucas Ramos Romagnoli <lucasr.romagnoli@gmail.com>
2020-01-27 23:56:03 -0200

    Acrescentando aos ExceptionsHandlers as exceptions de InteralServerError e BadRequest

--- flatland-web/src/main/java/br/com/lucasromagnoli/flatland/web/controller/errors/ErrorHandlerRestController.java
+++ flatland-web/src/main/java/br/com/lucasromagnoli/flatland/web/controller/errors/ErrorHandlerRestController.java
@@ -1,5 +1,7 @@
 package br.com.lucasromagnoli.flatland.web.controller.errors;
 
+import br.com.lucasromagnoli.javaee.underpinning.commons.exception.UnderpinningBadRequestException;
+import br.com.lucasromagnoli.javaee.underpinning.commons.exception.UnderpinningInternalServerErrorException;
 import br.com.lucasromagnoli.javaee.underpinning.commons.exception.UnderpinningValidationException;
 import br.com.lucasromagnoli.javaee.underpinning.rest.model.MessageType;
 import br.com.lucasromagnoli.javaee.underpinning.rest.model.TemplateMessage;
@@ -9,11 +11,35 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+/**
+ * @author github.com/lucasromagnoli
+ * @since 27/01/2020
+ */
 @RestControllerAdvice
 public class ErrorHandlerRestController {
 
+    @ExceptionHandler(UnderpinningBadRequestException.class)
+    public ResponseEntity<TemplateMessage> handleBadRequestException(UnderpinningBadRequestException ex) {
+        return TemplateMessageSupport.begin()
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .messageType(MessageType.ERROR)
+                .message(ex.getMessage())
+                .build()
+                .toResponseEntity();
+    }
+
+    @ExceptionHandler(UnderpinningInternalServerErrorException.class)
+    public ResponseEntity<TemplateMessage> handleInternalServerErrorException(UnderpinningInternalServerErrorException ex) {
+        return TemplateMessageSupport.begin()
+                .httpStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+                .messageType(MessageType.ERROR)
+                .message(ex.getMessage())
+                .build()
+                .toResponseEntity();
+    }
+
     @ExceptionHandler(UnderpinningValidationException.class)
-    public ResponseEntity<TemplateMessage> handleExcpetion(UnderpinningValidationException ex){
+    public ResponseEntity<TemplateMessage> handleValidationException(UnderpinningValidationException ex){
         return TemplateMessageSupport.begin()
                 .httpStatus(HttpStatus.BAD_REQUEST)
                 .messageType(MessageType.WARNING)
